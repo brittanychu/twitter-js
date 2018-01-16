af9a489b153c8156bd804b4afeeeb4942ab7b125
Veronica Smith <veronica.smith2009@yahoo.com>
2018-01-16 11:38:08 -0500

    up to ex-template

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-const express = require( 'express' );
-const app = express(); // creates an instance of an express application
+const app = require('express')();
+const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 
-app.use('/', function(req, res, next){
+var locals = {
+    title: 'example template',
+    people: [
+        { name: 'Brittany S. Chu' },
+        { name: 'Veronica S. Smith' }
+    ]
+};
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+nunjucks.configure('views', { noCache: true });
+nunjucks.render('index.html', locals, function(err, output) {
+    console.log(output);
+});
+
+app.use('/', function(req, res, next) {
     console.log('running root');
     next();
 });
 
-app.use('/news', function (req, res, next){
+app.use('/news', function(req, res, next) {
     console.log('running subpages')
     next();
 });
 
-app.get('/', function(req, res, next){
+app.get('/', function(req, res, next) {
     res.send('Hello World!')
 });
 
-app.get('/news', function(req, res, next){
+app.get('/ex-template', function(req, res, next) {
+    res.render('index', locals)
+});
+
+app.get('/news', function(req, res, next) {
     res.send('This is the news!')
 })
 
-if (app.listen(3000)) {console.log("server listening")};
+if (app.listen(3000)) { console.log("server listening") };
