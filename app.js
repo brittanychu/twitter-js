7ca90b624f64d0202904bbd8ef519119276441c9
Brittany Chu <brittany.s.chu@gmail.com>
2018-01-16 13:29:21 -0500

    committing twitter-js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const volleyball = require('volleyball');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
 
-
-
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views');
@@ -24,6 +22,9 @@ app.use('/news', function(req, res, next) {
 });
 
 app.use(express.static('public'));
+
+
+
 // app.get('/stylesheets/style.css', function(req, res, next) {
 //         res.sendFile('/stylesheets/style.css');
 
