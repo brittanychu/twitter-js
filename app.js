bf62f793ec1074aafbfadb4477c39ef6b77bc3b4
Brittany Chu <brittany.s.chu@gmail.com>
2018-01-16 10:16:18 -0500

    committing app

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
+app.use('/', function(req, res, next){
+    console.log('running root');
+    next();
+});
 
+app.use('/news', function (req, res, next){
+    console.log('running subpages')
+    next();
+});
+
+app.get('/', function(req, res, next){
+    res.send('Hello World!')
+});
+
+app.get('/news', function(req, res, next){
+    res.send('This is the news!')
+})
 
 if (app.listen(3000)) {console.log("server listening")};
