2df0d5d0b719a358a7edd7ddff1e5476779dd019
Veronica Smith <veronica.smith2009@yahoo.com>
2018-01-16 15:43:00 -0500

    tweeting by id

--- tweetBank.js
+++ tweetBank.js
@@ -2,8 +2,14 @@ var _ = require('lodash');
 
 var data = [];
 
+
 function add(name, content) {
-    data.push({ name: name, content: content });
+    var id = uniqueId();
+    data.push({ id: id, name: name, content: content });
+}
+
+const uniqueId = function() {
+    return data.length.toString();
 }
 
 function list() {
@@ -20,6 +26,7 @@ const randArrayEl = function(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 };
 
+
 const getFakeName = function() {
     const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
     const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
@@ -34,3 +41,5 @@ const getFakeTweet = function() {
 for (let i = 0; i < 10; i++) {
     module.exports.add(getFakeName(), getFakeTweet());
 };
+
+module.exports.add("Grace Hopper", "Whats up");
